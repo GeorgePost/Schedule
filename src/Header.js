9349747a04e8ede91c2d693bfcfbd748c5b0c579
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-04 07:46:17 -0400

    Started work on the login page

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,26 @@
 import logo from './images/skyline-high-resolution-logo.png'
+import { useState } from 'react';
 function Header() {
+  const [login,setLogin]=useState(false);
   return (
     <>
       <div className="header">
         <div className="Logo">
-            <img src={logo}/>
+          <img src={logo}/>
         </div>
         <div className='Title'>Schedule</div>
         <div className="User">
-            <div>Log In</div>
+          <div onClick={setLogin(true)}>Log In</div>
         </div>
       </div>
+      {
+        login && (
+        <div className="background">
+          <div>
+            Login
+          </div>
+        </div>)
+      }
     </>
   );
 }
